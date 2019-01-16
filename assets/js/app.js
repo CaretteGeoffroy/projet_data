30494d75d5db6409b3618370d8e0123308f30cad
GeoffroyCarette <geoffroy.c@codeur.online>
2019-01-16 18:38:03 +0100

    Ajout des avions et tri de l'affichage par Pays

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,6 @@
 require('../scss/main.scss');
 
 var mymap = L.map('mapid').setView([47.115, 2.548828], 6);
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
-var marker = L.marker([47.5, -0.09]).addTo(mymap);
-var marker = L.marker([51.618017, 9.84375]).addTo(mymap);
 
 
 L.tileLayer('https://api.mapbox.com/styles/v1/geoffroycarette/cjqxkkqxb15fm2rlqvssrl8r6/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
@@ -12,8 +9,28 @@ L.tileLayer('https://api.mapbox.com/styles/v1/geoffroycarette/cjqxkkqxb15fm2rlqv
     accessToken: 'pk.eyJ1IjoiZ2VvZmZyb3ljYXJldHRlIiwiYSI6ImNqcXh1c20ycTBiZm80M2tlMnBlazFsc3QifQ.Nj35s8pJZFDudfEJGWHpDA'
 }).addTo(mymap);
 
-function onMapClick(e) {
-    alert("You clicked the map at " + e.latlng);
-}
+setInterval(() => {
+fetch("https://opensky-network.org/api/states/all")
+    .then((res) => {
+        return res.json();
+    })
+    .then((res) => {
 
-mymap.on('click', onMapClick);
+            for (let i = 0; i < res.states.length; i++) {
+                if (res.states[i][2] == 'France') {
+                    if (res.states[i][5] != null || res.states[i][6] != null) {
+                        posA = res.states[i][5];
+                        posB = res.states[i][6];
+                        var marker = L.marker([posB, posA]).addTo(mymap);
+                    }
+                }
+    
+            }
+            console.log('Coucou');
+       
+
+    })
+    .catch((err) => {
+        if (err) throw err
+    })
+}, 3000);

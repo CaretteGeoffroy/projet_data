ed63ce3f8d2adf3c20d77fd1eeb6e6e1ab9d36e5
GeoffroyCarette <geoffroy.c@codeur.online>
2019-01-21 16:33:23 +0100

    Réinitialisation des markers sur la carte après changement de selection

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,8 @@
 require('../scss/main.scss');
 
-const map = L.map('mapid').setView([47.115, 2.548828], 6);
-
-let unixDateAjd = Math.round((new Date()).getTime() / 1000);
-let unixDateHier = Math.round(((new Date()).getTime() / 1000) - 1);
-
-
-let d = new Date();
-Math.round(d.setDate(d.getDate() - 1) / 1000);
-
-
-console.log(unixDateAjd);
-console.log(d);
 
 
 
-L.tileLayer('https://api.mapbox.com/styles/v1/geoffroycarette/cjqxkkqxb15fm2rlqvssrl8r6/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoiZ2VvZmZyb3ljYXJldHRlIiwiYSI6ImNqcXh1c20ycTBiZm80M2tlMnBlazFsc3QifQ.Nj35s8pJZFDudfEJGWHpDA'
-}).addTo(map);
 
 function fetchData() {
     return fetch("https://opensky-network.org/api/states/all")
@@ -27,7 +10,6 @@ function fetchData() {
             return res.json();
         })
         .then((res) => {
-            
             if( allCountry.length === 0 ){
                 res['states'].forEach( (element)=>{
                     if( element[2].length >  0 ){
@@ -38,9 +20,9 @@ function fetchData() {
                 createListDeroulante( allCountry );
             }
 
-
+            console.log(stateValue);
             return res.states.filter((state) => {
-                return (state[2] === 'France') && (state[5]) && (state[6]);
+                return (state[2] === stateValue) && (state[5]) && (state[6]);
             });
         })
         .catch((err) => {
@@ -59,7 +41,8 @@ function plotStates(map, markers) {
                 markers[icao24].setLatLng([lat, lng]);
             } else {
                 markers[icao24] = L.marker([lat, lng]);
-                markers[icao24].addTo(map);
+                markers[icao24].addTo(markersLayer);
+                map.addLayer(markersLayer);
             }
         });
         setTimeout(() => plotStates(map, markers), 15000);
@@ -78,4 +61,25 @@ function createListDeroulante(allCountry){
 
 const markers = {};
 var allCountry = [];
+const map = L.map('mapid').setView([47.115, 2.548828], 6);
+var markersLayer = new L.LayerGroup();
+let stateValue;
+L.tileLayer('https://api.mapbox.com/styles/v1/geoffroycarette/cjqxkkqxb15fm2rlqvssrl8r6/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiZ2VvZmZyb3ljYXJldHRlIiwiYSI6ImNqcXh1c20ycTBiZm80M2tlMnBlazFsc3QifQ.Nj35s8pJZFDudfEJGWHpDA'
+}).addTo(map);
+
+
+
 plotStates(map, markers);
+
+document.addEventListener('DOMContentLoaded',function() {
+    document.querySelector('select[name="country"]').onchange=changeEventHandler;
+},false);
+
+function changeEventHandler(event) {
+    map.removeLayer(markersLayer);
+    markersLayer = new L.LayerGroup();
+    stateValue = event.target.value;
+}
